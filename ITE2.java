4a8f874374a1e69190a568be7acda188a21ab939
Janani010 <113532819+Janani010@users.noreply.github.com>
2022-10-12 08:16:28 +0530

    Add files via upload

---
+++ ITE2.java
@@ -0,0 +1,27 @@
+class ITE2
+{
+
+int    salary;
+String itename;
+int    time;
+
+public ITE2(int s,String name,int t)
+{
+System.out.println(s  +"\n"+  name  +"\n"+  t);
+
+
+salary   = s;
+itename = name;
+time = t;
+
+}
+public ITE2(int s,String name)
+{
+
+System.out.println(s+"\n"+name);
+
+salary   = s;
+itename = name;
+
+}
+}
---
+++ Mnc2.java
@@ -0,0 +1,15 @@
+class Mnc2{
+
+public static void main(String args[])
+{
+ITE2 emp1 = new ITE2(10000,"janani",9);
+ITE2 emp2 = new ITE2(12000,"aaaaa",9);
+ITE2 emp3 = new ITE2(10000,"bahhnn",9);
+
+
+
+ITE2 emp7 = new ITE2(10000,"vhhhhh");
+
+}
+
+}
